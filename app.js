53484c65a2b449fc2b4b938e2f1c6f827baa716a
redwaterfall <gunnarodfoss@gmail.com>
2021-09-21 21:05:04 +0200

    made changes to speed variabel

--- app.js
+++ app.js
@@ -4,13 +4,15 @@ let speedText = document.querySelector('.speed');
 let latText = document.querySelector('.lat');
 let lonText = document.querySelector('.lon');
 
+
+
 navigator.geolocation.watchPosition(
     position => {
-        if(position.speed == undefined){
+        if(position.coords.speed == null){
             speedText.innerHTML = `${0}`;
         }
         else{
-            speedText.innerHTML = `${position.speed}`;
+            speedText.innerHTML = `${(position.coords.speed*3.6).toFixed(2)}`;
         }
         latText.innerHTML = `${position.coords.latitude.toFixed(2)}`;
         lonText.innerHTML = `${position.coords.longitude.toFixed(2)}`;
